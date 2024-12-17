86c13d1a0e1853ef02ad466a1cbd123fc590f606
nazmulnahid-git <nazmulnahid.official@gmail.com>
2024-12-17 22:46:12 +0600

    Add Icon components for Delete and Search; refactor SearchInput to use IconSearch from Icons.jsx

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -2,32 +2,7 @@ import React from 'react';
 import { StyleSheet, TextInput, View } from 'react-native';
 import { hp, wp } from '@/helpers/common';
 import { theme } from '../constants/theme';
-import Svg, { Path } from 'react-native-svg';
-
-const IconSearch = ({ width = 24, height = 24, color = '#000000', ...props }) => (
-  <Svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    width={width}
-    height={height}
-    fill="none"
-    {...props}
-  >
-    <Path
-      d="M17.5 17.5L22 22"
-      stroke={color}
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <Path
-      d="M20 11C20 6.02944 15.9706 2 11 2C6.02944 2 2 6.02944 2 11C2 15.9706 6.02944 20 11 20C15.9706 20 20 15.9706 20 11Z"
-      stroke={color}
-      strokeWidth="1.5"
-      strokeLinejoin="round"
-    />
-  </Svg>
-);
+import { IconSearch } from '../assets/icons/Icons';
 
 const Input = ({
   placeholder = 'Search',
@@ -42,11 +17,9 @@ const Input = ({
     <View style={[styles.container, style]}>
       {icon && (
         <IconSearch
-          width={24}
-          height={24}
+          width={wp(7)}
+          height={hp(7)}
           color={theme.colors.gray}
-          accessibilityLabel="Search Icon"
-          {...iconProps}
         />
       )}
       <TextInput
