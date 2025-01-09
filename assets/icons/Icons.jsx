8b8c8ea39de6e47a721ed0cfe8f202729075b099
nazmulnahid-git <nazmulnahid.official@gmail.com>
2025-01-09 23:10:14 +0600

    Add new Icon components: Profile, Email, and Password; update Icons.jsx for enhanced user interaction

--- assets/icons/Icons.jsx
+++ assets/icons/Icons.jsx
@@ -220,6 +220,7 @@ export const IconSearch = ({
   </Svg>
 );
 
+
 export const IconBack = ({
   width,
   height,
@@ -243,6 +244,115 @@ export const IconBack = ({
       strokeLinejoin="round"
     />
     <Path d="M8.99963 17C8.99963 17 3.99968 13.3176 3.99966 12C3.99965 10.6824 8.99966 7 8.99966 7"
+      stroke={color || defaultProps.color}
+      strokeWidth={strokeWidth || defaultProps.strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </Svg>
+);
+
+export const IconProfile = ({
+  width,
+  height,
+  color,
+  strokeWidth,
+  ...props
+}) => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={width || defaultProps.width}
+    height={height || defaultProps.height}
+    color={color || defaultProps.color}
+    fill="none"
+    {...props}
+  >
+    <Path
+      d="M6.57757 15.4816C5.1628 16.324 1.45336 18.0441 3.71266 20.1966C4.81631 21.248 6.04549 22 7.59087 22H16.4091C17.9545 22 19.1837 21.248 20.2873 20.1966C22.5466 18.0441 18.8372 16.324 17.4224 15.4816C14.1048 13.5061 9.89519 13.5061 6.57757 15.4816Z"
+      stroke={color || defaultProps.color}
+      strokeWidth={strokeWidth || defaultProps.strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <Path
+      d="M16.5 6.5C16.5 8.98528 14.4853 11 12 11C9.51472 11 7.5 8.98528 7.5 6.5C7.5 4.01472 9.51472 2 12 2C14.4853 2 16.5 4.01472 16.5 6.5Z"
+      stroke={color || defaultProps.color}
+      strokeWidth={strokeWidth || defaultProps.strokeWidth}
+    />
+  </Svg>
+);
+
+
+export const IconEmail = ({ width, height, color, strokeWidth, ...props }) => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={width || defaultProps.width}
+    height={height || defaultProps.height}
+    color={color || defaultProps.color}
+    fill="none"
+    {...props}
+  >
+    <Path
+      d="M2 6L8.91302 9.91697C11.4616 11.361 12.5384 11.361 15.087 9.91697L22 6"
+      stroke={color || defaultProps.color}
+      strokeWidth={strokeWidth || defaultProps.strokeWidth}
+      strokeLinejoin="round"
+    />
+    <Path
+      d="M2.01577 13.4756C2.08114 16.5412 2.11383 18.0739 3.24496 19.2094C4.37608 20.3448 5.95033 20.3843 9.09883 20.4634C11.0393 20.5122 12.9607 20.5122 14.9012 20.4634C18.0497 20.3843 19.6239 20.3448 20.7551 19.2094C21.8862 18.0739 21.9189 16.5412 21.9842 13.4756C22.0053 12.4899 22.0053 11.5101 21.9842 10.5244C21.9189 7.45886 21.8862 5.92609 20.7551 4.79066C19.6239 3.65523 18.0497 3.61568 14.9012 3.53657C12.9607 3.48781 11.0393 3.48781 9.09882 3.53656C5.95033 3.61566 4.37608 3.65521 3.24495 4.79065C2.11382 5.92608 2.08114 7.45885 2.01576 10.5244C1.99474 11.5101 1.99475 12.4899 2.01577 13.4756Z"
+      stroke={color || defaultProps.color}
+      strokeWidth={strokeWidth || defaultProps.strokeWidth}
+      strokeLinejoin="round"
+    />
+  </Svg>
+);
+
+export const IconPassword = ({
+  width,
+  height,
+  color,
+  strokeWidth,
+  ...props
+}) => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={width || defaultProps.width}
+    height={height || defaultProps.height}
+    color={color || defaultProps.color}
+    fill="none"
+    {...props}
+  >
+    <Path
+      d="M4.26781 18.8447C4.49269 20.515 5.87613 21.8235 7.55966 21.9009C8.97627 21.966 10.4153 22 12 22C13.5847 22 15.0237 21.966 16.4403 21.9009C18.1239 21.8235 19.5073 20.515 19.7322 18.8447C19.879 17.7547 20 16.6376 20 15.5C20 14.3624 19.879 13.2453 19.7322 12.1553C19.5073 10.485 18.1239 9.17649 16.4403 9.09909C15.0237 9.03397 13.5847 9 12 9C10.4153 9 8.97627 9.03397 7.55966 9.09909C5.87613 9.17649 4.49269 10.485 4.26781 12.1553C4.12105 13.2453 4 14.3624 4 15.5C4 16.6376 4.12105 17.7547 4.26781 18.8447Z"
+      stroke={color || defaultProps.color}
+      strokeWidth={strokeWidth || defaultProps.strokeWidth}
+    />
+    <Path
+      d="M7.5 9V6.5C7.5 4.01472 9.51472 2 12 2C14.4853 2 16.5 4.01472 16.5 6.5V9"
+      stroke={color || defaultProps.color}
+      strokeWidth={strokeWidth || defaultProps.strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <Path
+      d="M16 15.49V15.5"
+      stroke={color || defaultProps.color}
+      strokeWidth={strokeWidth || defaultProps.strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <Path
+      d="M12 15.49V15.5"
+      stroke={color || defaultProps.color}
+      strokeWidth={strokeWidth || defaultProps.strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <Path
+      d="M8 15.49V15.5"
       stroke={color || defaultProps.color}
       strokeWidth={strokeWidth || defaultProps.strokeWidth}
       strokeLinecap="round"
