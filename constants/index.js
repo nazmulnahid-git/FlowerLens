03e037f744e638dc3ffe734bca07110fc01ab4af
nazmulnahid-git <nazmulnahid.official@gmail.com>
2025-02-07 00:05:01 +0600

    Update image selection and add Cloudinary configuration
    - Enable base64 image encoding in GalleryView and CameraView
    - Modify selectedImage state to store full image object
    - Update image rendering to use selectedImage.uri
    - Add Cloudinary cloud name and upload preset to constants

--- constants/index.js
+++ constants/index.js
@@ -1,2 +1,4 @@
 export const supabaseUrl = 'https://vlloixcjgkmptnxmeyte.supabase.co';
 export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsbG9peGNqZ2ttcHRueG1leXRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg0Mjg1MDQsImV4cCI6MjA1NDAwNDUwNH0.j8SONXkAe4tWs5iAOpcY-TNzqRgWDEdk7aNHfJXzgOE'
+export const cloudinaryCloudName = 'drifdisby';
+export const cloudinaryUploadPreset = 'flowerlens';
